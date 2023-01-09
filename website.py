a7043894fd1431df813709f09c14014397dea8cf
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2023-01-09 11:25:05 +0530

    routes has begun

--- website.py
+++ website.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1>Home</h1>"
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
